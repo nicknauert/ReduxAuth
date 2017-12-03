3544652296a59a05d0db8495a11df23679b52c91
Nick Nauert <nicknauert@gmail.com>
2017-12-03 18:20:02 -0500

    prepping jwt

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,10 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user){
+  return jwt.encode({  }, config.secret);
+}
 
 exports.signup = function( req, res, next){
   const email = req.body.email;
