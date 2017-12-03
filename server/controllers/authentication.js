886bca60386c05b0ba4c4c8c93f04ebd999f6da7
Nick Nauert <nicknauert@gmail.com>
2017-12-03 18:49:46 -0500

    beginning Passport

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -3,7 +3,8 @@ const jwt = require('jwt-simple');
 const config = require('../config');
 
 function tokenForUser(user){
-  return jwt.encode({  }, config.secret);
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
 exports.signup = function( req, res, next){
@@ -30,7 +31,7 @@ exports.signup = function( req, res, next){
     user.save(function(err){
       if (err) return next(err);
       // Respond to request indicating user was created
-      res.json({ success: 'true' });
+      res.json({ token: tokenForUser(user) });
     });
   });
 }
