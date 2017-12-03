b42cda7883c194dfc2642e72dfd2a63b381eccf3
Nick Nauert <nicknauert@gmail.com>
2017-12-03 17:06:59 -0500

    post route, doing user creation

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication')
+
 module.exports = function(app){
-  app.get('/', function(req, res){
-    res.send(["Bacon", "bacon", "bacon"]);
-  })
+  app.post('/signup', Authentication.signup)
 }
