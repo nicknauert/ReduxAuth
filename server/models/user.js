6aed00126da62de08815a2666ec493e9580dfef2
Nick Nauert <nicknauert@gmail.com>
2017-12-03 17:44:39 -0500

    correctly hashing passwords

--- server/models/user.js
+++ server/models/user.js
@@ -10,15 +10,19 @@ const userSchema = new Schema({
 
 
 // On Save Hook, encrypt password
+// Before saving a model, run this function
 userSchema.pre('save', function(next){
   const user = this;
 
+  // Gen a salt, then run callback
   bcrpyt.genSalt( 10, function(err, salt){
     if (err) return next(err);
 
-    bcrpyt.hash(user.password, salt.null, function(err, hash){
+    // Hash the pass using salt
+    bcrpyt.hash(user.password, salt, null, function(err, hash){
       if (err) return next(err);
 
+      // overwrite plain text password with encrypted password
       user.password = hash;
       next();
     })
