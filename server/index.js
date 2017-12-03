d975878e053971ce1bb4957b6001981d109c3510
Nick Nauert <nicknauert@gmail.com>
2017-12-03 16:50:35 -0500

    continuing route

--- server/index.js
+++ server/index.js
@@ -4,18 +4,18 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const mognoose = require('mongoose');
 const app = express();
-
 const router = require('./router.js')
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth')
 
 // App setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
-
-
 //Server setup
-
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
