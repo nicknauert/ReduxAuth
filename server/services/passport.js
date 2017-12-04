c7154efddfdd5ce1e01920365b7563d4c1dd97f2
Nick Nauert <nicknauert@gmail.com>
2017-12-04 10:22:59 -0500

    tokens creating, middleware for checking for tokens

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,3 +3,29 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup options for JWT strat
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+// Create JWT Strat
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
+  // See if user ID in payload exists in DB
+  // true -> call done with that user
+  // false -> call done w/o user object
+  User.findById(payload.sub, function(err, user){
+    if ( err ) { return done(err, false); }
+
+    if ( user ){
+      done( null, user);
+    } else {
+      done( null, false );
+    }
+  })
+});
+
+// Tell Passport to use this strat
+
+passport.use(jwtLogin);
