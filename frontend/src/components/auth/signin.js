237581bd5860f4ffdec709ea834ededf8bf500fa
Nick Nauert <nicknauert@gmail.com>
2017-12-11 12:59:40 -0500

    form showing

--- frontend/src/components/auth/signin.js
+++ frontend/src/components/auth/signin.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-import reduxForm from 'redux-form';
+import { reduxForm } from 'redux-form';
 
 class Signin extends Component {
+  handleFormSubmit({email, password}) {
+    console.log(email, password);
+  }
+  
   render(){
+    const { handleSubmit, fields: {email, password}} = this.props;
 
+    return(
+      <form onSubmit={handleSubmit(this.handleFormSubmit).bind(this)} >
+        <fieldset className="form-group">
+          <label>Email</label>
+          <input {...email} className="form-control"/>
+          <label>Password</label>
+          <input {...password} className="form-control"/>
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign In</button>
+      </form>
+    )
 
   }
 };
