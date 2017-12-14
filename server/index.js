add4c7c8c918c92bbbb8be0c012eaae599e207a1
Nick Nauert <nicknauert@gmail.com>
2017-12-14 16:25:16 -0500

    minor server changes, JWT token delivering

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const mognoose = require('mongoose');
 const app = express();
 const router = require('./router.js')
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth')
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
