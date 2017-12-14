1d4ced40127ed8c122e0048d2a188a6cba095d04
Nick Nauert <nicknauert@gmail.com>
2017-12-14 14:08:24 -0500

    beginning thunk implimentation

---
+++ frontend/src/actions/index.js
@@ -0,0 +1,17 @@
+
+
+export function signinUser({ email, password }) {
+  // submit email/pass to server
+
+  // if req is good
+  // - update state ti indicate user is authed
+  // - save JWT token
+  // - redirect to /feature
+
+  //if req is bad..
+  // - show error to user
+  
+
+
+
+}
