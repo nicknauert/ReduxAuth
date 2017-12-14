eb15d9c7642d43fe89512f8db8f90592e394a646
Nick Nauert <nicknauert@gmail.com>
2017-12-14 16:42:10 -0500

    auth actions scaffold

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -6,14 +8,19 @@ export function signinUser({ email, password }) {
   console.log(email, password);
   return function(dispatch){
     // submit email/pass to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-    // if req is good
-    // - update state ti indicate user is authed
-    // - save JWT token
-    // - redirect to /feature
-    
-    //if req is bad..
-    // - show error to user
-
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+          .then( res => {
+            // if req is good
+            // - update state ti indicate user is authed
+            dispatch({ type: AUTH_USER,  }) 
+            // - save JWT token
+            // - redirect to /feature
+            browserHistory.push('/feature')
+            
+          })
+          .catch(() => {
+            //if req is bad..
+            // - show error to user
+          })
   }
 }
