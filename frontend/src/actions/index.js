a58d59d9e32e942978569b375acc6aa95e164c8d
Nick Nauert <nicknauert@gmail.com>
2017-12-14 14:33:29 -0500

    thunk progress

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,17 +1,19 @@
+import axios from 'axios';
 
+const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
-  // submit email/pass to server
+  return function(dispatch){
+    // submit email/pass to server
+    axios.post(`${ROOT_URL}/signin`, { email, password });
+    // if req is good
+    // - update state ti indicate user is authed
+    // - save JWT token
+    // - redirect to /feature
+    
+    //if req is bad..
+    // - show error to user
+    
 
-  // if req is good
-  // - update state ti indicate user is authed
-  // - save JWT token
-  // - redirect to /feature
-
-  //if req is bad..
-  // - show error to user
-  
-
-
-
+  }
 }
