f65a38d65a8680d57044769b6a55f3e86a04653e
Nick Nauert <nicknauert@gmail.com>
2017-12-15 10:55:34 -0500

    header updating

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
-export default class Header extends Component {
+class Header extends Component {
+  renderLinks(){
+    if(this.props.authenticated){
+      return <li className="nav-item">
+        <Link to="/signout" className="nav-link">Sign Out</Link>
+      </li>
+    } else {
+      return [
+        <li className="nav-item" key='signin'>
+          <Link to="/signin" className="nav-link">Sign In</Link>
+        </li>,
+        <li className="nav-item" key='signup'>
+          <Link to="/signup" className="nav-link">Sign Up</Link>
+        </li>
+      ]
+    }
+  }
   render(){
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
         <ul className="nav navbar-nav">
           <li className="nav-item">
-            Sign In
+            <Link className="nav-link" to="/feature">Feature</Link>
           </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated }
+}
+
+export default connect(mapStateToProps)(Header);
