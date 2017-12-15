c8d27fc7ffd6a786399e549473a18c4621074228
Nick Nauert <nicknauert@gmail.com>
2017-12-15 10:05:37 -0500

    displaying login erros on screen

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import {
+  AUTH_USER,
+  AUTH_ERROR
+} from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -9,19 +12,27 @@ export function signinUser({ email, password }) {
   return function(dispatch){
     // submit email/pass to server
     axios.post(`${ROOT_URL}/signin`, { email, password })
-          .then( res => {
-            // if req is good
-            // - update state ti indicate user is authed
-            dispatch({ type: AUTH_USER  }) 
-            // - save JWT token
-            localStorage.setItem("JWT_TOKEN", res.data.token);
-            // - redirect to /feature
-            browserHistory.push('/feature')
-            
-          })
-          .catch(() => {
-            //if req is bad..
-            // - show error to user
-          })
+      .then( res => {
+        // if req is good
+        // - update state ti indicate user is authed
+        dispatch({ type: AUTH_USER  }) 
+        // - save JWT token
+        localStorage.setItem("JWT_TOKEN", res.data.token);
+        // - redirect to /feature
+        browserHistory.push('/feature')
+        
+      })
+      .catch(() => {
+        //if req is bad..
+        // - show error to user
+        dispatch(authError('Bad Login Info'));
+      })
+  }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
   }
 }
--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -1,6 +1,7 @@
 import {
   AUTH_USER,
-  UNAUTH_USER
+  UNAUTH_USER,
+  AUTH_ERROR
 } from '../actions/types';
 
 export default function( state = {}, action) {
@@ -10,6 +11,9 @@ export default function( state = {}, action) {
 
     case UNAUTH_USER:
       return { ...state, authenticated: false };
+    
+    case AUTH_ERROR:
+      return { ...state, error: action.payload };
   }
 
   return state;
