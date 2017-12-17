4d03de67fb835522bec7d41805b48d9c8a1b3431
Nick Nauert <nicknauert@gmail.com>
2017-12-17 13:55:31 -0500

    handling browser nav while unauthed

---
+++ frontend/src/components/feature.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Feature extends Component {
+  render(){
+    return(
+      <div>FEATURE</div>
+    );
+  }
+}
+
+export default Feature;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
+import Feature from './components/feature';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
@@ -15,11 +16,12 @@ const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory}>
-      <Route path='/' component={App}> 
-        <Route path='/signin' component={Signin}/>
-        <Route path='/signout' component={Signout}/>
+    <Router history={browserHistory} >
+      <Route path='/' component={App} > 
+        <Route path='/signin' component={Signin} />
+        <Route path='/signout' component={Signout} />
         <Route path='/signup' component={Signup} />
+        <Route path='/feature' component={Feature} />
       </Route>
     </Router>
   </Provider>
