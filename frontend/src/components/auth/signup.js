5ea951f7aef544b3a0981b7a5e8c98fe9b99ee9e
Nick Nauert <nicknauert@gmail.com>
2017-12-17 12:38:10 -0500

    beginning of validating sign up form

--- frontend/src/components/auth/signup.js
+++ frontend/src/components/auth/signup.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
-import reduxForm from 'redux-form';
-import { connect } from 'react-redux';
+import {reduxForm} from 'redux-form';
+import * as actions from '../../actions'
 
 
 class Signup extends Component {
-  render(){
+  render() {
+    const { handleSubmit, fields: {email, password, passwordConfirm}} = this.props;
     return (
-      <form onSubmit={} >
+      <form >
         <fieldset className="form-group">
-          <label>Email</label>
+          <label>Email:</label>
           <input {...email} className="form-control"/>
-          <label>Password</label>
-          <input {...password} type='password' className="form-control"/>
         </fieldset>
-        {this.renderAlert()}
-        <button action="submit" className="btn btn-primary">Sign In</button>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input {...password} type='password' className="form-control"/>
+          {password.touched && password.error && <div className="error">{password.error}</div>}
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input {...passwordConfirm} type='password' className="form-control"/>
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign Up</button>
       </form>
     )
   }
 }
 
-export default Signup;
+function validate(formProps) {
+  const errors = {};
+
+  if (formProps.password !== formProps.passwordConfirm){
+    errors.password = "Passwords must match."
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
+})(Signup);
